551d8d27e935b10e2a340c08be9cfa61f22b536f
Xabi <xabi.aycaguer@gmail.com>
2023-03-30 21:58:27 +0200

    :white_check_mark: add rspec test

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Event, type: :model do
+  subject {
+    described_class.new(
+      name: "Super New Event",
+      start_date: 10.days.from_now,
+      end_date: 12.days.from_now,
+    )
+  }
+
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a start_date" do
+      subject.start_date = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a end_date" do
+      subject.end_date = nil
+      expect(subject).to_not be_valid
+    end
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+    subject {
+      described_class.new(
+        first_name: "Sarah",
+        last_name: "Connor",
+        email: "sarah.connor@cant_die.com",
+        password: "password"
+      )
+    }
+
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is valid with valid without first_name" do
+      expect(subject).to be_valid
+    end
+
+    it "is valid with valid without last_name" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a email" do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a password" do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid with existing email" do
+      described_class.create(
+        email: "sarah.connor@cant_die.com",
+        password: "password"
+      )
+      expect(subject).to_not be_valid
+    end
+  end
+end
